f4ec6b8178ae7ecc0d0849900242d7f0a0bdc834
Joël Weber <joel@joelweber.nl>
2021-02-21 01:22:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import './App.css';
 
 function App() {
+  const { transcript, resetTranscript } = useSpeechRecognition()
+
+  useEffect(() => {
+    SpeechRecognition.startListening({ language: 'nl-NL', continuous: true })
+  }, [])
+
+  const countMan = () => {
+    const re = /[M|m]an/g
+    return ((transcript || '').match(re) || []).length
+  }
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>We ondersteunen deze browser niet 😐</p>
+          <p>Hint: probeer chrome</p>
+        </header>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{countMan()}</p>
       </header>
     </div>
   );
